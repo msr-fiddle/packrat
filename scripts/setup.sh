217b4554ba43ce2acfb5807f46c7ed237621aa11
Ankit Bhardwaj <bhrdwj.ankit@gmail.com>
2022-05-31 17:00:52 -0400

    Add a setup script.

---
+++ scripts/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+install_torch() {
+    pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
+}
+
+install_deps() {
+    sudo apt update --yes
+    sudo apt install python3-pip --yes
+    sudo apt-get install python-is-python3
+}
+
+# Check the number of arguments
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <deps|torch>"
+  exit 1
+fi
+
+if [ "$1" == "deps" ]; then
+  install_deps
+elif [ "$1" == "torch" ]; then
+  install_torch
+else
+  echo "Unknown argument: $1"
+  exit 1
+fi
