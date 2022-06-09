3c50e4f3e67fca23dcb25622c2fb136cbc940d84
Ankit Bhardwaj <bhrdwj.ankit@gmail.com>
2022-06-09 15:35:59 -0700

    Update setup script to install vtune.

--- README.md
+++ README.md
@@ -16,3 +16,13 @@ python run.py
 docker build -t bench .
 docker run -v `pwd`:/app -it bench:latest
 ```
+
+To run VTune inside docker image
+
+```bash
+docker run --pid=host --cap-add=SYS_ADMIN --cap-add=SYS_PTRACE -v `pwd`:/app -it bench:latest
+```
+
+Reference: 
+- [Run VTune Profiler in a Container](https://www.intel.com/content/www/us/en/develop/documentation/vtune-help/top/launch/containerization-support/run-from-container.html)
+- [Install VTune with the Packet Managers](https://www.intel.com/content/www/us/en/develop/documentation/vtune-install-guide/top/linux/package-managers.html)
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,10 +5,27 @@ set -e
 if [ "$EUID" -ne 0 ]; then
     echo "not running as root, using sudo"
     APT="sudo apt-get"
+    SUDO="sudo"
 else
     echo "running as root."
     APT="apt-get"
+    SUDO=""
 fi
+
+APPEND="--yes --no-install-recommends"
+
+install_vtune() {
+    $APT install wget gnupg $APPEND
+    $APT install kmod software-properties-common pkg-config $APPEND
+    wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+    $SUDO apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB 2>/dev/null
+    rm GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB || true
+    $SUDO add-apt-repository "deb https://apt.repos.intel.com/oneapi all main"
+
+    $APT update --yes
+    $APT install linux-headers-generic $APPEND
+    $APT install intel-oneapi-vtune $APPEND
+}
 
 install_torch() {
     pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
@@ -17,12 +34,12 @@ install_torch() {
 }
 
 install_deps() {
-    APPEND="--yes --no-install-recommends"
     $APT update --yes
     $APT install python3-pip $APPEND
     $APT install python-is-python3 python3-autopep8 pylint $APPEND
     $APT install numactl $APPEND
     $APT install gcc python3-dev $APPEND
+    $APT install make build-essential $APPEND
 }
 
 # Check the number of arguments
@@ -35,7 +52,9 @@ if [ "$1" == "deps" ]; then
   install_deps
 elif [ "$1" == "torch" ]; then
   install_torch
+elif [ "$1" == "vtune" ]; then
+  install_vtune
 else
-  echo "Unknown argument: $1"
+  echo "Usage: $0 <deps|torch|vtune>"
   exit 1
 fi
