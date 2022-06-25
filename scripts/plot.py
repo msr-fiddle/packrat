58132cf5d4b61091822e66e1178f721660a08317
Ankit Bhardwaj <bhrdwj.ankit@gmail.com>
2022-06-25 16:22:50 -0600

    Update the plot script.

--- scripts/plot.py
+++ scripts/plot.py
@@ -8,6 +8,7 @@ from plotnine.themes import theme_gray, theme
 from plotnine.themes.elements import (element_line, element_rect,
                                       element_text, element_blank)
 from plotnine import ggplot
+from plotnine.labels import ggtitle
 from plotnine.mapping import aes
 from plotnine.labels import labs
 from plotnine.scales import scale_x_continuous, scale_y_continuous, scale_color_discrete, scale_y_log10
@@ -38,45 +39,118 @@ class MyTheme(theme_gray):
             inplace=True)
 
 
-def plot_benchmark_latency(benchmarks):
+def plot_benchmark_latency_per_batch(benchmarks):
     "Plots a latency graph for every data-structure in the results file"
 
-    benchmarks['config'] = benchmarks.apply(
-        lambda row: "Batch={:d}".format(int(row.batch_size)), axis=1)
+    for batch_size in benchmarks['batch_size'].unique():
+        data_set = []
+        for name in benchmarks.benchmark.unique():
+            opt = name.split('_')[2].upper()
+            benchmark = benchmarks.loc[(benchmarks['benchmark'] == name) &
+                                       (benchmarks['batch_size'] == batch_size)]
+            benchmark.insert(0, 'config', "opt={}".format(opt))
+            data_set.append(benchmark)
+        data = pd.concat(data_set)
 
-    plot = ggplot(data=benchmarks, mapping=aes(x='threads', y='latency', color='config')) + \
-        MyTheme(base_size=10) + labs(y="Latency [ms]") + \
-        theme(legend_position='top', legend_title=element_blank()) + \
-        scale_x_continuous(
-            breaks=benchmarks['threads'].unique(), labels=["{}".format(thr) for thr in benchmarks['threads'].unique()], name='# Threads') + \
-        scale_y_log10(labels=lambda lst: ["{:.1f}".format(x) for x in lst]) + \
-        scale_color_discrete(breaks=benchmarks['config'].unique()) + \
-        geom_point() + \
-        geom_line()
+        plot = ggplot(data=data, mapping=aes(x='threads', y='latency(avg)', color='config')) + \
+            MyTheme(base_size=10) + labs(y="Latency [ms]") + \
+            theme(legend_position='top', legend_title=element_blank(), legend_box_spacing=-0.35) + \
+            ggtitle("Threads vs Latency (BS={})".format(batch_size)) + \
+            scale_x_continuous(
+                breaks=data['threads'].unique(), labels=["{}".format(thr) for thr in data['threads'].unique()], name='# threads') + \
+            scale_y_log10(labels=lambda lst: ["{:.1f}".format(x) for x in lst]) + \
+            scale_color_discrete(breaks=data['config'].unique()) + \
+            geom_point() + \
+            geom_line()
 
-    plot.save("resnet-latency.png", dpi=300, width=12, height=5, verbose=False)
+        plot.save("resnet-latency-{}.png".format(batch_size), dpi=300,
+                  width=12, height=5, verbose=False)
 
 
-def plot_benchmark_throughputs(benchmarks):
-    "Plots a throughput graph for every data-structure in the results file"
-    # Fail if we have more than one experiment duration
+def plot_benchmark_latency_per_opt(benchmarks):
+    "Plots a latency graph for every data-structure in the results file"
 
-    benchmarks['config'] = benchmarks.apply(
-        lambda row: "Batch={:d}".format(int(row.batch_size)), axis=1)
+    for name in benchmarks.benchmark.unique():
+        data_set = []
+        for batch_size in benchmarks['batch_size'].unique():
+            opt = name.split('_')[2]
+            benchmark = benchmarks.loc[(benchmarks['benchmark'] == name) &
+                                       (benchmarks['batch_size'] == batch_size)]
+            benchmark.insert(0, 'config', "Batch={}".format(batch_size))
+            data_set.append(benchmark)
+        data = pd.concat(data_set)
 
-    plot = ggplot(data=benchmarks, mapping=aes(x='threads', y='throughput', color='config')) + \
-        MyTheme(base_size=10) + \
-        labs(y="Throughput [ops]") + \
-        theme(legend_position='top', legend_title=element_blank()) + \
-        scale_x_continuous(
-            breaks=benchmarks['threads'].unique(), labels=["{}".format(thr) for thr in benchmarks['threads'].unique()], name='# Threads') + \
-        scale_y_continuous(labels=lambda lst: ["{:,}".format(x) for x in lst]) + \
-        scale_color_discrete(breaks=benchmarks['config'].unique()) + \
-        geom_point() + \
-        geom_line()
+        plot = ggplot(data=data, mapping=aes(x='threads', y='latency(avg)', color='config')) + \
+            MyTheme(base_size=10) + labs(y="Latency [ms]") + \
+            theme(legend_position='top', legend_title=element_blank(), legend_box_spacing=-0.35) + \
+            ggtitle("Threads vs Latency (Opt={})".format(opt.upper())) + \
+            scale_x_continuous(
+                breaks=data['threads'].unique(), labels=["{}".format(thr) for thr in data['threads'].unique()], name='# threads') + \
+            scale_y_log10(labels=lambda lst: ["{:.1f}".format(x) for x in lst]) + \
+            scale_color_discrete(breaks=data['config'].unique()) + \
+            geom_point() + \
+            geom_line()
 
-    plot.save("resnet-throughput.png", dpi=300,
-              width=12, height=5, verbose=False)
+        plot.save("resnet-latency-{}.png".format(opt), dpi=300,
+                  width=12, height=5, verbose=False)
+
+
+def plot_benchmark_throughputs_per_batch(benchmarks):
+    "plots a throughput graph for every data-structure in the results file"
+
+    for batch_size in benchmarks['batch_size'].unique():
+        data_set = []
+        for name in benchmarks.benchmark.unique():
+            opt = name.split('_')[2].upper()
+            benchmark = benchmarks.loc[(benchmarks['benchmark'] == name) &
+                                       (benchmarks['batch_size'] == batch_size)]
+            benchmark.insert(0, 'config', "opt={}".format(opt))
+            data_set.append(benchmark)
+        data = pd.concat(data_set)
+
+        plot = ggplot(data=data, mapping=aes(x='threads', y='throughput', color='config')) + \
+            MyTheme(base_size=10) + \
+            labs(y="throughput [ops]") + \
+            theme(legend_position='top', legend_title=element_blank(), legend_box_spacing=-0.35) + \
+            ggtitle("Threads vs Throughput (BS={})".format(batch_size)) + \
+            scale_x_continuous(
+                breaks=data['threads'].unique(), labels=["{}".format(thr) for thr in data['threads'].unique()], name='# threads') + \
+            scale_y_continuous(labels=lambda lst: ["{:,}".format(x) for x in lst]) + \
+            scale_color_discrete(breaks=data['config'].unique()) + \
+            geom_point() + \
+            geom_line()
+
+        plot.save("resnet-throughput-{}.png".format(batch_size), dpi=300,
+                  width=12, height=5, verbose=False)
+
+
+def plot_benchmark_throughputs_per_opt(benchmarks):
+    "plots a throughput graph for every data-structure in the results file"
+
+    for name in benchmarks.benchmark.unique():
+        data_set = []
+        for batch_size in benchmarks['batch_size'].unique():
+            opt = name.split('_')[2]
+            benchmark = benchmarks.loc[(benchmarks['benchmark'] == name) &
+                                       (benchmarks['batch_size'] == batch_size)]
+            benchmark.insert(0, 'config', "Batch={}".format(batch_size))
+            data_set.append(benchmark)
+        data = pd.concat(data_set)
+
+        plot = ggplot(data=data, mapping=aes(x='threads', y='throughput', color='config')) + \
+            MyTheme(base_size=10) + \
+            labs(y="throughput [ops]") + \
+            theme(legend_position='top', legend_title=element_blank(), legend_box_spacing=-0.35) + \
+            ggtitle("Threads vs Throughput (Opt={})".format(opt.upper())) + \
+            scale_x_continuous(
+                breaks=data['threads'].unique(), labels=["{}".format(thr) for thr in data['threads'].unique()], name='# threads') + \
+            scale_y_continuous(labels=lambda lst: ["{:,}".format(x) for x in lst]) + \
+            scale_color_discrete(breaks=data['config'].unique()) + \
+            geom_point() + \
+            geom_line()
+
+        plot.save("resnet-throughput-{}.png".format(opt), dpi=300,
+                  width=12, height=5, verbose=False)
 
 
 if __name__ == '__main__':
@@ -87,9 +161,11 @@ if __name__ == '__main__':
     DF = pd.read_csv(sys.argv[2], skip_blank_lines=True)
 
     if sys.argv[1] == "latency":
-        plot_benchmark_latency(DF)
+        plot_benchmark_latency_per_batch(DF)
+        plot_benchmark_latency_per_opt(DF)
     elif sys.argv[1] == "throughput":
-        plot_benchmark_throughputs(DF)
+        plot_benchmark_throughputs_per_batch(DF)
+        plot_benchmark_throughputs_per_opt(DF)
     else:
         print("Unknown benchmark type")
         sys.exit(1)
