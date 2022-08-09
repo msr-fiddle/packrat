e31dea65a518938ab96cc844ce4432fa486952b4
Ankit Bhardwaj <bhrdwj.ankit@gmail.com>
2022-08-09 15:27:51 -0600

    Serialize optimizer results.

--- utils/optimizer.py
+++ utils/optimizer.py
@@ -13,8 +13,50 @@ latency[t',b'] = latency of processing a batch with b' inputs with t' threads (f
 
 from io import BytesIO
 import urllib.request
-import numpy as np
+import pickle
 import pandas as pd
+
+
+def get_data(model: str, benchmark: str):
+    """
+    Get the data from the benchmark.
+    :param model: model name
+    :param benchmark: benchmark type latency or throughput
+    :return: dataframe
+    """
+    base = "https://msr-fiddle.github.io/naf"
+    url = f"{base}/{model}/skylake2x/large-batches/{model}_{benchmark}.csv"
+    print(url)
+    print(f" > Fetching {url} ...", end='')
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(f"OK ({response.getcode()}).")
+            data = pd.read_csv(BytesIO(response.read()))
+            return data
+    except urllib.error.HTTPError as error:
+        print(f"FAILED ({error.code}).")
+        return None
+
+
+def format_data(data):
+    """
+    Format the data into a matrix.
+    :param data: dataframe
+    :return: matrix
+    """
+    data = data.loc[:, ["intraop_threads", "batch_size", "latency(avg)"]]
+
+    latencies = {}
+    for thread in data.intraop_threads.unique():
+        batch_latencies = {}
+        for batch in data.batch_size.unique():
+            latency = data.loc[(data["intraop_threads"] == thread) &
+                               (data["batch_size"] == batch)]["latency(avg)"].values[0]
+            batch_latencies[batch] = latency
+
+        latencies[thread] = batch_latencies
+
+    return latencies
 
 
 class Optimizer:
@@ -22,51 +64,23 @@ class Optimizer:
     Configuration optimizer for the benchmark.
     """
 
-    def get_data(self, model: str, benchmark: str):
-        """
-        Get the data from the benchmark.
-        :param model: model name
-        :param benchmark: benchmark type latency or throughput
-        :return: dataframe
-        """
-        base = "https://msr-fiddle.github.io/naf"
-        url = f"{base}/{model}/skylake2x/large-batches/{model}_{benchmark}.csv"
-        print(url)
-        print(f" > Fetching {url} ...", end='')
-        try:
-            with urllib.request.urlopen(url) as response:
-                print(f"OK ({response.getcode()}).")
-                data = pd.read_csv(BytesIO(response.read()))
-                return data
-        except urllib.error.HTTPError as error:
-            print(f"FAILED ({error.code}).")
-            return None
-
-    def format_data(self, data):
-        """
-        Format the data into a matrix.
-        :param data: dataframe
-        :return: matrix
-        """
-        data = data.loc[:, ["intraop_threads", "batch_size", "latency(avg)"]]
-
-        latencies = {}
-        for thread in data.intraop_threads.unique():
-            batch_latencies = {}
-            for batch in data.batch_size.unique():
-                latency = data.loc[(data["intraop_threads"] == thread) &
-                                   (data["batch_size"] == batch)]["latency(avg)"].values[0]
-                batch_latencies[batch] = latency
-
-            latencies[thread] = batch_latencies
-
-        return latencies
-
     def __init__(self):
         """Initialize the optimizer."""
-        self.latencies = self.format_data(self.get_data("resnet", "latency"))
+        self.opt = {}
+        self.latencies = {}
+        self.workloads = ["resnet", "inception", "gpt2", "bert"]
+        for workload in self.workloads:
+            self.latencies[workload] = format_data(
+                get_data(workload, "latency"))
+            try:
+                with open(f'utils/{workload}.pickle', 'rb') as file:
+                    self.opt[workload] = pickle.load(file)
+            except FileNotFoundError:
+                self.opt[workload] = self.min_latency(workload)
+                with open(f'{workload}.pickle', 'wb') as file:
+                    pickle.dump(self.opt[workload], file)
 
-    def min_latency(self, max_threads, max_batch, latency):
+    def min_latency(self, workload):
         """
         Given a set of threads and batches, find the minimum latency.
         :param max_threads: maximum number of threads
@@ -74,6 +88,10 @@ class Optimizer:
         :param latency: matrix of latencies
         :return: optimial latency matrix
         """
+
+        latency = self.latencies[workload]
+        max_threads = max(latency.keys())
+        max_batch = max(latency[max_threads].keys())
 
         opt = [[float('inf') for _ in range(max_batch + 1)]
                for _ in range(max_threads + 1)]
@@ -91,38 +109,29 @@ class Optimizer:
                             opt[thread][batch] = min(
                                 opt[thread][batch], latency[thread][batch])
 
-        # print(f"latency { latency}")
-        # np.set_printoptions(linewidth=np.inf)
-        # print(f"opt { np.array(opt, dtype=np.float16)}")
         return opt
 
-    def solution(self, threads, batch, latency, opt, result):
+    def solution(self, threads, batch, workload, result):
         if threads <= 0 or batch <= 0:
             return 0
 
+        latency = self.latencies[workload]
+        opt = self.opt[workload]
         max_threads, max_batch = 0, 0
-        for t in range(threads, 0, -1):
-            for b in range(batch, 0, -1):
-                if t in latency and b in latency[t]:
-                    if opt[threads][batch] == opt[t][b] == latency[t][b]:
-                        max_threads, max_batch = threads - t, batch - b
-                        result.append((t, b))
+
+        for t_i in range(threads, 0, -1):
+            for b_i in range(batch, 0, -1):
+                if t_i in latency and b_i in latency[t_i]:
+                    if opt[threads][batch] == opt[t_i][b_i] == latency[t_i][b_i]:
+                        max_threads, max_batch = threads - t_i, batch - b_i
+                        result.append((t_i, b_i))
                         break
 
-        return self.solution(max_threads, max_batch, latency, opt, result)
+        return self.solution(max_threads, max_batch, workload, result)
 
 
 if __name__ == "__main__":
     optimizer = Optimizer()
-    assert optimizer.min_latency(
-        3, 3, np.array([[0, 0, 0, 0], [0, 7, 8, 9], [0, 4, 5, 6], [0, 1, 2, 3]]))[3][3] == 3
-
-    max_threads = max(optimizer.latencies.keys())
-    max_batch = max(optimizer.latencies[max_threads].keys())
-    opt = optimizer.min_latency(
-        max_threads, max_batch, optimizer.latencies)
-
     instances = []
-    optimizer.solution(max_threads, max_batch,
-                       optimizer.latencies, opt, instances)
+    optimizer.solution(18, 1024, "resnet", instances)
     print(instances)
