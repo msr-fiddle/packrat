e4a6019cc5944da6a7ecbd28a6cc6bf2ff0fab26
Ankit Bhardwaj <bhrdwj.ankit@gmail.com>
2022-09-10 18:17:26 -0600

    Add a basic model cache to Packrat.

--- benchmarks/cache/store.py
+++ benchmarks/cache/store.py
@@ -1,47 +1,95 @@
+import pickle
+from pyarrow import plasma
+import pyarrow as pa
 
-def get_model(name: str):
+# PyTorch model
+import torch
+from torchvision import models
+import transformers
+
+from serde import extract_tensors, pack_frames, replace_tensors, unpack_frames
+
+
+def get_model(name: str) -> torch.nn.Module:
+
     if name == "resnet50":
-        import torchvision.models as models
-        model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
-        model.eval()
-        return model
-    elif name == "inception":
-        import torchvision.models as models
-        model = models.inception_v3(
+        resnet50 = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+        resnet50.eval()
+        return resnet50
+
+    if name == "inception":
+        inception = models.inception_v3(
             weights=models.Inception_V3_Weights.DEFAULT)
-        model.eval()
-        return model
-    elif name == "bert":
-        import transformers
+        inception.eval()
+        return inception
+
+    if name == "bert":
         transformers.logging.set_verbosity_error()
-        model = transformers.BertModel.from_pretrained('bert-base-uncased')
-        model.eval()
-        return model
-    elif name == "gpt2":
-        import transformers
-        model = transformers.GPT2Model.from_pretrained('gpt2')
-        model.eval()
-        return model
-    else:
-        raise Exception("Unknown model: {}".format(name))
+        bert = transformers.BertModel.from_pretrained('bert-base-uncased')
+        bert.eval()
+        return bert
+
+    if name == "gpt2":
+        gpt2 = transformers.GPT2Model.from_pretrained('gpt2')
+        gpt2.eval()
+        return gpt2
+
+    raise Exception(f"Unknown model: {name}")
 
 
 class Cache:
+    """
+    Zero-copy model transfer from the cache to the client
+    """
+
     def __init__(self):
-        self.cache = {}
-        self.benchmarks = ["resnet50", "bert", "gpt2", "inception"]
-        for benchmark in self.benchmarks:
-            self.cache[benchmark] = get_model(benchmark)
+        """
+        Cache for storing models and their weights
+        params:
+            storename: name of the plasma store
+            benchmark: name of the benchmark
+            buf_id: id of the out-of-band buffer
+            serial: id of the serialized model
+        """
+        self.store = plasma.start_plasma_store(1 * 1024 * 1024 * 1024)
+        self.storename, _ignore = self.store.__enter__()
+        client = plasma.connect(self.storename)
+
+        self.benchmark = "resnet50"
+        ouf_of_band_buffer = []
+        model = get_model(self.benchmark)
+        serialized_model = pickle.dumps(
+            extract_tensors(model),
+            buffer_callback=lambda b: ouf_of_band_buffer.append(b.raw()),
+            protocol=pickle.HIGHEST_PROTOCOL
+        )
+
+        _sz, byte_ls = pack_frames(ouf_of_band_buffer)
+        buf = pa.py_buffer(b"".join(byte_ls))
+
+        self.buf_id = client.put(buf)
+        self.serial = client.put(serialized_model)
+        client.disconnect()
+
+        # TODO: Figure out what to do with the buffers (fixme)
+        del ouf_of_band_buffer
+        del model
+        del serialized_model
+        del buf
+        del byte_ls
+
+    def __str__(self):
+        return self.__class__.__name__
+
+    def get_model(self) -> torch.nn.Module:
+        client = plasma.connect(self.storename)
+        (model, weights) = pickle.loads(client.get(self.serial),
+                                        buffers=unpack_frames(client.get(self.buf_id)))
+        replace_tensors(model, weights)
+        return model
 
 
 if __name__ == '__main__':
     cache = Cache()
-    import ray
-    ray.put(cache)
-    from pyarrow import plasma
-    client = plasma.connect("/tmp/plasma")
-    client.put(cache)
-    print(cache.cache["resnet50"])
-    # print(cache.cache["bert"])
-    # print(cache.cache["gpt2"])
-    # print(cache.cache["inception"])
+    resnet = cache.get_model()
+    resnet.eval()
