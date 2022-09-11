2391e7b13863ea8caac7ae070bb8e790b279dd85
Ankit Bhardwaj <bhrdwj.ankit@gmail.com>
2022-09-10 18:17:26 -0600

    Add a basic model cache to Packrat.

--- benchmarks/cache/serde.py
+++ benchmarks/cache/serde.py
@@ -1,19 +1,7 @@
-from io import IOBase
 from typing import Tuple, List, Dict
 import copy
+import struct
 import torch
-import pickle
-
-
-class Serde(IOBase):
-    def __init__(self):
-        self.data = list()
-
-    def write(self, model):
-        self.data.append(model)
-
-    def read(self, __size=10) -> bytes:
-        return bytes(self.data)
 
 
 def extract_tensors(m: torch.nn.Module) -> Tuple[torch.nn.Module, List[Dict]]:
@@ -49,11 +37,9 @@ def extract_tensors(m: torch.nn.Module) -> Tuple[torch.nn.Module, List[Dict]]:
 
 def replace_tensors(m: torch.nn.Module, tensors: List[Dict]):
     """
-    Restore the tensors that extract_tensors() stripped out of a 
-    PyTorch model.
-    :param no_parameters_objects: Skip wrapping tensors in 
-     ``torch.nn.Parameters`` objects (~20% speedup, may impact
-     some models)
+    Restore the tensors that extract_tensors() stripped out of a PyTorch model.
+    :param no_parameters_objects: Skip wrapping tensors in ``torch.nn.Parameters``
+    objects (~20% speedup, may impact some models)
     """
     modules = [module for _, module in m.named_modules()]
     for module, tensor_dict in zip(modules, tensors):
@@ -65,16 +51,54 @@ def replace_tensors(m: torch.nn.Module, tensors: List[Dict]):
             module.register_buffer(name, torch.as_tensor(array))
 
 
-def restore_from_cache(model_and_tensors_ref):
-    import ray
-    model, tensors = ray.get(model_and_tensors_ref)
-    replace_tensors(model, tensors)
-    return model
+def nbytes(frame, _bytes_like=(bytes, bytearray)):
+    if isinstance(frame, _bytes_like):
+        return len(frame)
+
+    try:
+        return frame.nbytes
+    except AttributeError:
+        return len(frame)
 
 
-if __name__ == '__main__':
-    import torchvision.models as models
-    model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
-    serde = Serde()
-    pickle.dump(extract_tensors(model), serde)
-    ser_model = pickle.load(serde)
+def pack_frames_prelude(frames):
+    """Pack the `frames` metadata."""
+    lengths = [struct.pack("Q", len(frames))] + [
+        struct.pack("Q", nbytes(frame)) for frame in frames
+    ]
+    return b"".join(lengths)
+
+
+def pack_frames(frames):
+
+    prelude = [pack_frames_prelude(frames)]
+
+    if not isinstance(frames, list):
+        frames = list(frames)
+
+    data_ls = prelude + frames
+    data_sz = sum(map(lambda b: len(b), data_ls))
+    return data_sz, data_ls
+
+
+def unpack_frames(b):
+    """Unpack bytes into a sequence of frames.
+
+    This assumes that length information is at the front of the bytestring,
+    as performed by pack_frames
+
+    See Also
+    --------
+    pack_frames
+    """
+    (n_frames,) = struct.unpack("Q", b[:8])
+
+    frames = []
+    start = 8 + n_frames * 8
+    for i in range(n_frames):
+        (length,) = struct.unpack("Q", b[(i + 1) * 8: (i + 2) * 8])
+        frame = b[start: start + length]
+        frames.append(frame)
+        start += length
+
+    return frames
